eade7ac90751e7ec82bbbe922b860a56f7978ad3
Cyril <cyril.bier42100@gmail.com>
2024-11-27 10:41:56 +0100

    Routing + projects component init

--- rattatask-front/src/app/app.routes.ts
+++ rattatask-front/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component:LoginComponent},
+  {path:'projects',component:ProjectsComponent}
+];
--- rattatask-front/src/app/login/login.component.ts
+++ rattatask-front/src/app/login/login.component.ts
@@ -4,11 +4,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatCardModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
+  imports: [
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -18,7 +24,7 @@ export class LoginComponent {
     password: new FormControl(''),
   });
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   onSubmit(): void {
     if (this.form.valid) {
@@ -26,6 +32,7 @@ export class LoginComponent {
       this.authService.login(email, password).subscribe((isAuthenticated) => {
         if (isAuthenticated) {
           console.log('Login successful');
+          this.router.navigate(['/projects']);
         } else {
           console.log('Login failed');
         }
