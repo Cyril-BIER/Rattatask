cfd16c8b03843fc1e72ac4d9928a383edccf1608
Cyril <cyril.bier42100@gmail.com>
2024-11-28 15:05:26 +0100

    Add requestMatcher to allow preflight request

--- taskManager/src/main/java/com/cbi/taskManager/configs/SecurityConfiguration.java
+++ taskManager/src/main/java/com/cbi/taskManager/configs/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.cbi.taskManager.configs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,19 +29,21 @@ public class SecurityConfiguration {
                         csrf
                                 .ignoringRequestMatchers(toH2Console())
                                 .disable())
-                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
                 .authorizeHttpRequests(auth->
                         auth.requestMatchers("/auth/**").permitAll()
                                 .requestMatchers("/swagger-ui/**").permitAll()
                                 .requestMatchers("/v3/api-docs/**").permitAll()
                                 .requestMatchers(toH2Console()).permitAll()
+                                .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .headers(headers-> headers
-                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin
-                        ))
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .headers(headers-> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+
 
         return http.build();
     }
