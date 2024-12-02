7c1733497a30130c267f514839e8b545f20d3e83
Cyril <cyril.bier42100@gmail.com>
2024-12-02 11:16:19 +0100

    Improve task component

--- rattatask-front/src/app/components/task/task.component.ts
+++ rattatask-front/src/app/components/task/task.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Task } from '../../interfaces/Task';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
