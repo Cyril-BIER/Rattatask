28246dd2fa3551c7aafcf12a99f7c07de372fb3e
Cyril <cyril.bier42100@gmail.com>
2024-12-03 16:44:18 +0100

    GREEN and refactored get1UserTest

--- rattataskAPI/src/main/java/com/cbi/taskManager/service/UserService.java
+++ rattataskAPI/src/main/java/com/cbi/taskManager/service/UserService.java
@@ -1,13 +1,20 @@
 package com.cbi.taskManager.service;
 
 import com.cbi.taskManager.dto.UserDTO;
+import com.cbi.taskManager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
     public List<UserDTO> getUsers(List<Long> ids) {
-        return null;
+        return userRepository.findAllById(ids).stream()
+                .map(u->new UserDTO(u.getId(),u.getUsername()))
+                .toList();
     }
 }
--- rattataskAPI/src/test/java/com/cbi/taskManager/service/UserServiceTestCase.java
+++ rattataskAPI/src/test/java/com/cbi/taskManager/service/UserServiceTestCase.java
@@ -1,23 +1,33 @@
 package com.cbi.taskManager.service;
 
 import com.cbi.taskManager.dto.UserDTO;
+import com.cbi.taskManager.model.User;
+import com.cbi.taskManager.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTestCase {
+    @Mock
+    UserRepository userRepository;
 
-
+    @InjectMocks
     UserService userService = new UserService();
 
     @Test
     public void get1UserTest(){
         List<UserDTO> expected =List.of( new UserDTO(1L, "bob@mail.com"));
+        when(userRepository.findAllById(List.of(1L))).thenReturn(List.of(
+                new User(1L, "bob@mail.com", "password")
+        ));
         assertEquals(expected, userService.getUsers(List.of(1L)));
     }
 }
