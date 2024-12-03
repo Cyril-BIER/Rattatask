0d2c41c61e98ddec66e02189e1fa91267d60dbf0
Cyril <cyril.bier42100@gmail.com>
2024-12-03 16:48:27 +0100

    getManyUserTest

--- rattataskAPI/src/test/java/com/cbi/taskManager/service/UserServiceTestCase.java
+++ rattataskAPI/src/test/java/com/cbi/taskManager/service/UserServiceTestCase.java
@@ -30,4 +30,21 @@ public class UserServiceTestCase {
         ));
         assertEquals(expected, userService.getUsers(List.of(1L)));
     }
+
+    @Test
+    public void getManyUsersTest(){
+        List<UserDTO> expected =List.of(
+                new UserDTO(1L, "bob@mail.com"),
+                new UserDTO(3L, "alice@mail.com"),
+                new UserDTO(5L, "eve@mail.com")
+
+        );
+        when(userRepository.findAllById(List.of(1L,3L, 5L))).thenReturn(List.of(
+                new User(1L, "bob@mail.com", "password"),
+                new User(3L, "alice@mail.com", "password"),
+                new User(5L, "eve@mail.com", "password")
+
+        ));
+        assertEquals(expected, userService.getUsers(List.of(1L, 3L, 5L)));
+    }
 }
